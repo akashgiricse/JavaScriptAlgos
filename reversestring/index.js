b4e55f37e4e9054ae7612ebbb8b577156de7ecc2
akashgiricse <akashgiricse@gmail.com>
2019-06-21 11:20:33 +0530

    Reverse a string

--- reversestring/index.js
+++ reversestring/index.js
@@ -6,9 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-// Solution 1
-function reverse(str) {
-   return str.split('').reverse().join('');
+// Solution 1 (Using reverse function )
+// function reverse(str) {
+//    return str.split('').reverse().join('');
+// }
+
+// Solution 2 (Using for loop)
+function reverse(str){
+    let reversed = ''
+    for(let character of str) {
+        reversed = character + reversed
+    }
+    return reversed
 }
+
+// Solution 3 
+// function reverse(str) {
+//     return str.split('').reduce( (rev,char) => char + rev, '');
+// }
+
+reverse('alksl')
 
 module.exports = reverse;
